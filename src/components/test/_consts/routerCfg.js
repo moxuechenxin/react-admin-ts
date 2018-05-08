794222ba209e486cc650047776a99649ef81f2a0
moxuechenxin <1689819155@qq.com>
2018-05-08 17:30:12 +0800

    add test

---
+++ src/components/test/_consts/routerCfg.js
@@ -0,0 +1,10 @@
+// 当前模块名（路由名前缀）
+export const MOUDLE_PREDIX = 'test'
+
+export const INDEX = {
+  path: `/${MOUDLE_PREDIX}`,
+  meta: {
+    level: 0,
+    label: 'TEST管理'
+  }
+}
---
+++ src/components/test/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Moudle_test extends React.Component {
+  render() {
+    return (
+      <div id="module-test" >
+        2231000
+      </div>
+    )
+  }
+}
+
+export default Moudle_test
